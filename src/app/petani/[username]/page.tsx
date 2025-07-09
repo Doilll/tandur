eb041ded65595bb36d37ced259f83a87cdcb5915
Doilll <ahmdfadhoil57@gmail.com>
2025-07-09 18:17:43 +0700

    tes

---
+++ src/app/petani/[username]/page.tsx
@@ -0,0 +1,8 @@
+
+export default function PetaniPage() {
+    return (
+        <div className="flex h-full w-full items-center justify-center">
+        <h1 className="text-2xl font-bold text-slate-800">Halaman Petani</h1>
+        </div>
+    );
+}
