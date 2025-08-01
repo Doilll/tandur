6257f18336e91aeab1ae0aa975d29afa08c4e2a3
Doilll <ahmdfadhoil57@gmail.com>
2025-08-01 20:24:03 +0700

    not found page custom

---
+++ src/app/not-found.tsx
@@ -0,0 +1,35 @@
+import { Leaf, ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import Providers from "@/components/providers";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export default function NotFound() {
+  return (
+    <>
+      <Providers>
+        <Navbar />
+      </Providers>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-green-50 px-6 text-center">
+        <Leaf className="text-green-600 w-16 h-16 mb-4" />
+
+        <h1 className="text-5xl font-bold text-green-800 mb-2">
+          404 - Halaman Tidak Ditemukan
+        </h1>
+        <p className="text-gray-700 text-lg max-w-xl">
+          Sepertinya ladang yang kamu cari belum ditanami apa pun. Yuk kembali
+          ke halaman utama dan mulai menanam ide!
+        </p>
+
+        <Link
+          href="/"
+          className="mt-6 inline-flex items-center text-green-700 hover:text-green-900 font-semibold transition"
+        >
+          <ArrowLeft className="mr-2" />
+          Kembali ke Beranda
+        </Link>
+      </div>
+      <Footer />
+    </>
+  );
+}
